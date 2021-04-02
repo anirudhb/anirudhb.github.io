12b0ab4a0d52b321ee98527f9954922a6e435b37
anirudhb <anirudhb@users.noreply.github.com>
2021-04-02 16:07:49 -0700

    move themes out of source

--- engine/build.rs
+++ engine/build.rs
@@ -2,10 +2,10 @@ use std::path::Path;
 use syntect::highlighting::ThemeSet;
 
 fn main() -> anyhow::Result<()> {
-    println!("cargo:rerun-if-changed=src/themes");
+    println!("cargo:rerun-if-changed=themes");
     let dest_path = Path::new(&std::env::var_os("OUT_DIR").unwrap()).join("themes.themedump");
     let mut ts = ThemeSet::new();
-    ts.add_from_folder("src/themes")?;
+    ts.add_from_folder("themes")?;
     syntect::dumps::dump_to_file(&ts, dest_path)?;
     Ok(())
 }
